912f62308ac1834a0c56a7a665fdf47d81e7848a
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2023-10-26 07:04:54 +0800

    ♻ Refactor `index.js`
    - Renamed `listing` to `filteredItems` in
      the function `processResponse()`
    - Renamed `x` to `mapped` in the function `filterMatches()`
    - Renamed `items` to `itemsToRename` in the function `renameFiles()`
    - Renamed `e` to `entry` in the function `renameFiles()`
    - Removed nested-if by returning early if no items to rename in
      the function `renameFiles()`

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ dbx.filesSearchV2({ 'query': searchQurey, 'options': searchOptions })
 function processResponse(response) {
   const json = response.result;
   // List the items to be renamed
-  const listing = filterMatches(json.matches);
+  const filteredItems = filterMatches(json.matches);
   // Rename the items in the list
-  renameFiles(listing);
+  renameFiles(filteredItems);
   
   // Check if there is next page
   if (json.has_more) {
@@ -68,59 +68,68 @@ function processResponse(response) {
  * @returns {Object} List of the old and new path of the items to be renamed
  */
 function filterMatches(items) {
-  return items.map((item) => {
-    // Extract the name and path from each search result
-    const name = item.metadata.metadata.name;
-    const path = item.metadata.metadata.path_display;
+  return items
+    .map((item) => {
+      // Extract the name and path from each search result
+      const name = item.metadata.metadata.name;
+      const path = item.metadata.metadata.path_display;
 
-    // Generate the new name and new path
-    let newName = name;
-    renameRules.forEach(rule => {
-      newName = newName.replace(rule.pattern, rule.newString);
+      // Generate the new name and new path
+      let newName = name;
+      renameRules.forEach(rule => {
+        newName = newName.replace(rule.pattern, rule.newString);
+      })
+      const newPath = path.replace(name, newName);
+
+      return { from_path: path, to_path: newPath };
     })
-    const newPath = path.replace(name, newName);
-
-    return { from_path: path, to_path: newPath };
-  })
     // filter and return only the items need to be renamed by checking if any case insensitive changes
-    .filter(x => x.from_path.lower() !== x.to_path.lower());
+    .filter((mapped) => mapped.from_path.lower() !== mapped.to_path.lower());
 }
 
 /**
  * Rename the items by calling the MoveBatch V2 API
  * @param [Object] List of renaming items
  */
-function renameFiles(items) {
-  if (items.length > 0) {
-    // Submit a job
-    dbx.filesMoveBatchV2({ 'entries': items })
-      .then((response) => {
-        // Output the job status when checkProgress() returns a fulfilled Promise
-        console.log('\nStart to rename the following queries:');
-        checkProgress(response.result.async_job_id, items)
-          .then((result) => {
-            console.log(''); // Output an empty line
-            result.entries.forEach((e, i) => {
-              if (e['.tag'] === 'success') {
-                // Output a success message with the updated pathname
-                console.log(`Successfully renamed the ${e.success['.tag']} "${path.dirname(e.success.path_display)}/${e.success.name}"`);
-              } else if (e['.tag'] === 'failure') {
-                // Output a failure message with the original pathname
-                console.log(`Failed to rename "${items[i].from_path}".`);
-              } else {
-                // Output an error message from the JSON response if got something other than a success or failure
-                console.error('Unknown status: ' + JSON.stringify(e));
-              }
-            })            
-          })
-          .catch((err) => {
-            console.error(err);
-          });
-      })
-      .catch((err) => {
-        console.log(err);
-      })
+function renameFiles(itemsToRename) {
+  if (itemsToRename.length <= 0) {
+    console.log("0 items to reanme");
+    return;
   }
+
+  // Submit a job
+  dbx
+    .filesMoveBatchV2({ entries: itemsToRename })
+    .then((response) => {
+      // Output the job status when checkProgress() returns a fulfilled Promise
+      console.log("\nStart to rename the following queries:");
+      checkProgress(response.result.async_job_id, itemsToRename)
+        .then((result) => {
+          console.log(""); // Output an empty line
+          result.entries.forEach((entry, i) => {
+            if (entry[".tag"] === "success") {
+              // Output a success message with the updated pathname
+              console.log(
+                `Successfully renamed the ${entry.success[".tag"]} "${path.dirname(
+                  entry.success.path_display
+                )}/${entry.success.name}"`
+              );
+            } else if (entry[".tag"] === "failure") {
+              // Output a failure message with the original pathname
+              console.log(`Failed to rename "${itemsToRename[i].from_path}".`);
+            } else {
+              // Output an error message from the JSON response if got something other than a success or failure
+              console.error("Unknown status: " + JSON.stringify(entry));
+            }
+          });
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 }
 
 /**
